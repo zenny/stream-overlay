aa5227b2c8fe6cb1c78082e5d962f6cc8de8387e
Julien Yo <mangaultime@hotmail.fr>
2018-03-09 00:00:59 +0100

    .

--- uploadimg.php
+++
@@ -1,5 +0,0 @@
-<?php
-$sourcePath = $_FILES['file']['tmp_name'];
-$targetPath = "upload/img/".$_FILES['file']['name'];
-move_uploaded_file($sourcePath,$targetPath);
-?>
--- uploadjson.php
+++
@@ -1,7 +0,0 @@
-<?php
-$myFile = "upload/json/".$_POST['name'].".json";
-$fh = fopen($myFile, 'w') or die("impossible to open file");
-$stringData = $_POST['data'];
-fwrite($fh, $stringData);
-fclose($fh);
-?>
